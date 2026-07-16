f4887c4a65d40e50530ec5ed005df8c829c55dab
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2026-07-16 22:46:46 +0700

    Revert "Docker (#3)"
    This reverts commit fa2a7dd0f99047ac8dec24c80c3d21e2c0b065db.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vue from '@astrojs/vue';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
