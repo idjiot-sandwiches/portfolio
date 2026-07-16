9cf887d7015a8585e86c81f0747c5d2b6c029a7a
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2026-07-16 12:36:36 +0700

    Grid Contents (#1)
    * feat: resume redirect card
    
    * feat: each card as component
    
    * feat: time card
    
    * feat: summary card
    
    * feat: add funny img
    
    * fix: grid
    
    * feat: reorder & new grid
    
    * feat: tools & stacks
    
    * fix: summary redirect _blank
    
    * fix: background color
    
    * fix: minor
    
    * feat: update time
    
    * feat: add color palette card

---
+++ src/components/ui/card/CardTitle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import { cn } from "@/lib/utils"
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"]
+}>()
+</script>
+
+<template>
+  <h3
+    data-slot="card-title"
+    :class="cn('leading-none scroll-m-20 text-xl font-semibold tracking-tight', props.class)"
+  >
+    <slot />
+  </h3>
+</template>
